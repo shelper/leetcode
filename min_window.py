bcb8c45dd8be6109f0e2d65b0d6b3eac977ad52e
shelper <shelper@126.com>
2020-12-25 11:12:10 -0500

    working solution with dict

--- min_window.py
+++ min_window.py
@@ -1,8 +1,5 @@
-char2_idx = lambda c: ord(c) - ord("A")
-
-
 class Solution(object):
-    def minWindow(self, s, t):
+    def min_window(self, s, t):
         """
         :type s: str
         :type t: str
@@ -11,14 +8,14 @@ class Solution(object):
         if not t or len(s) < len(t):
             return ""
 
-        t_char_count = [0] * 256
-        substring_char_count = [0] * 256
+        t_char_count = {}
+        substring_char_count = {}
 
         for c in t:
-            t_char_count[char2_idx(c)] += 1
+            t_char_count[c] = t_char_count.get(c, 0) + 1
 
         begin, end = -1, -1
-        left, right = 0, -1
+        left, right = 0, 0
         len_t_in_s = 0
 
         while True:
@@ -27,33 +24,34 @@ class Solution(object):
                     break
                 else:
                     left += 1
-                    removed_char_idx = char2_idx(s[left - 1])
-                    if t_char_count[removed_char_idx] > 0:
-                        substring_char_count[removed_char_idx] -= 1
+                    removed_char = s[left - 1]
+                    if t_char_count.get(removed_char, 0) > 0:
+                        substring_char_count[removed_char] -= 1
                         if (
-                            substring_char_count[removed_char_idx]
-                            < t_char_count[removed_char_idx]
+                            substring_char_count[removed_char]
+                            < t_char_count[removed_char]
                         ):
                             len_t_in_s -= 1
 
             if len_t_in_s < len(t):
-                if right == len(s) - 1:
+                if right == len(s):
                     break
                 else:
                     right += 1
-                    added_char_idx = char2_idx(s[right])
-                    if t_char_count[added_char_idx] > 0:
-                        substring_char_count[added_char_idx] += 1
-                        if (
-                            substring_char_count[added_char_idx]
-                            <= t_char_count[added_char_idx]
+                    added_char = s[right - 1]  # the count - 1 is the index
+                    if t_char_count.get(added_char, 0) > 0:
+                        substring_char_count[added_char] = (
+                            substring_char_count.get(added_char, 0) + 1
+                        )
+                        if substring_char_count.get(added_char) <= t_char_count.get(
+                            added_char
                         ):
                             len_t_in_s += 1
 
             if len_t_in_s == len(t) and (end - begin > right - left or begin < 0):
                 begin, end = left, right
 
-        return s[begin : end + 1]
+        return s[begin:end]
 
 
-print(Solution().minWindow("ADOBECODEBANC", "ABC"))
+print(Solution().min_window("ADOBECODEBANC", "ABC"))
